d56c784122f726fbca5ff6d08a8584d55524b645
Thiagtao da Silva Adriano <tadriano@band.com.br>
2018-09-14 15:05:30 -0300

    dockerfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: '//producao.com'
+  API_URL: 'http://localhost:5200'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: '//desenvolvimento.com'
+  API_URL: 'http://localhost:5000'
 };
 
 /*
